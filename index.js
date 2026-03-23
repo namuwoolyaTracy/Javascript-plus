133800c1c22e1592d54a09ebb731326ec24bd237
namuwoolyaTracy <namuwoolyatracy1@gmail.com>
2026-03-23 12:51:11 +0300

    My first Javascript

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log("My name is Tracy");
+        
+ // variables
+//  declaring variales use the var, let, or const keywords
+
+// var is funcation-spaced and can be redeclared and updated
+
+var name = "Tracy";
+console.log(name);
+name = "Namuwoolya Tracy"
+console.log(name)
+
+// let is block-scopped and can be updated but not redeclared
+
+let age = 23
+console.log(age);
+age = 24;
+console.log("My age changed because Iam just a girl", age);
+
+const country = "Uganada";
+console.log(country);
+
+
+// Data types
+// Javascript  has several data types including
+// 1. string
+let greeting = "Hello World, How are you?";
+console.log(typeof greeting);
+
+// 2. number
+
+let x = 20;
+let y = 11;
+console.log(x)
+console.log(y)
+console.log(typeof x)
+console.log(typeof y)
